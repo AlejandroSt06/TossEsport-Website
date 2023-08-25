f4ca4a5a21e34d32fc05bfc8d81640b27bc3211c
Mirko <mirkosaavedra596@gmail.com>
2023-08-25 12:41:50 +0200

    adsdasdfewfdfsad

--- src/Components/HomeEventCalendar.jsx
+++ src/Components/HomeEventCalendar.jsx
@@ -1,10 +1,10 @@
 function EventCalendar(){
 
-    const sectionImage = "https://cdn.dribbble.com/users/43577/screenshots/15139657/keyboard_powder_1600x1200_1.png?resize=450x338&vertical=center"
+    const sectionImage = "https://i.pinimg.com/736x/3a/dd/3f/3add3fee70905528c05a37c78b8e982f.jpg"
 return(
 
 
- <div className="container col-xxl-8 px-4 py-5 event-calendar">
+ <div className="container col-xxl-8 px-4 py-1 event-calendar">
             <div className="row flex-lg-row align-items-center g-5 py-4 my-5 pb-3">
                 <div className="col-10 col-sm-8 col-lg-6">
                     <img src={sectionImage} className="event-calendar-image d-block mx-lg-auto img-fluid" alt="hero-image" width="900" height="100" loading="lazy" />
