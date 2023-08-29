64294c2a07d0446327f4d1f5da21e7182ec13072
Mirko <mirkosaavedra596@gmail.com>
2023-08-29 12:37:19 +0200

    hello

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -21,7 +21,13 @@ function Header(){
     <div className='col-0 col-lg-1'>
    
     </div>
-    
+             <form className="col-12 d-none d-xxl-flex  col-lg-auto mb-3 mb-lg-0 me-lg-0 ms-4 ps-0" role="search">
+
+<Search></Search>
+</form>
+    <div className='col-0 col-lg-1'>
+   
+   </div>
         <ul className=" nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 top-header-nav">
           <li><a href="/" className="nav-link px-2 ">Home</a></li>
           <li><a href="/eventos" className="nav-link px-2 ">Eventos</a></li>
@@ -30,16 +36,13 @@ function Header(){
           <li><a href="#" className="nav-link px-2 text-secondary">About</a></li>
         </ul>
         <div className='col-0 col-lg-auto'></div>
-         <form className="col-12 d-none d-xxl-flex  col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
 
-<Search></Search>
-</form>
        
 
-        <div className="text-end col-lg-auto d-none d-lg-flex">
+        {/* <div className="text-end col-lg-auto d-none d-lg-flex">
           <button type="button" className="btn login-btn me-2">Login</button>
           <button type="button" className="btn sign-up-btn">Sign-up</button>
-        </div>
+        </div> */}
         
       </div>
     </div>
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,10 +1,78 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
-
+import "./contact.css"
 function Contact(){
+  const sectionImage = "https://i.pinimg.com/736x/3a/dd/3f/3add3fee70905528c05a37c78b8e982f.jpg";
     return (
     <>
     <Header/>
+    <div className='background-dark px-0 mx-0 '>
+    <div class="container">
+  <main className="py-1 mt-1 row">
+
+    <div class="row g-5 me-0 py-5 px-lg-5">
+
+      <div class=" col-md-7 col-lg-8 mx-lg-5">
+        <h4 class="mb-3">Contactanos!</h4>
+        <form class="needs-validation" novalidate="">
+        
+          <div class="row g-3">
+            <div class="col-12">
+              <label for="firstName" class="form-label">Nombre</label>
+              <input type="text" class="form-control" id="firstName" placeholder="" value="" required="" />
+              <div class="invalid-feedback">
+                Valid first name is required.
+              </div>
+            </div>
+
+           
+
+
+
+            <div class="col-12 justify-content-center">
+              <label for="email" class="form-label">Email <span class="text-body-secondary">(Optional)</span></label>
+              <input type="email" class="form-control" id="email" placeholder="you@example.com" />
+              <div class="invalid-feedback">
+                Please enter a valid email address for shipping updates.
+              </div>
+            </div>
+
+            <div class="col-12">
+              <label for="address" class="form-label">Mensaje</label>
+              <textarea className="form-control" rows = "5"></textarea>
+              <div class="invalid-feedback">
+                Please enter your shipping address.
+              </div>
+            </div>
+
+          </div>
+
+
+
+
+
+
+          <button class="w-100 btn-lg my-5 contact-sub-button" type="submit">Enter</button>
+        </form>
+      </div>
+      <div className="col-12">dasds</div>
+    </div>
+   
+  </main>
+<div>
+
+
+</div>
+  <footer class="my-5 pt-5 text-body-secondary text-center text-small">
+    <p class="mb-1">© 2017–2023 Company Name</p>
+    <ul class="list-inline">
+      <li class="list-inline-item"><a href="#">Privacy</a></li>
+      <li class="list-inline-item"><a href="#">Terms</a></li>
+      <li class="list-inline-item"><a href="#">Support</a></li>
+    </ul>
+  </footer>
+</div>
+    </div>
     </>
     )
 }
